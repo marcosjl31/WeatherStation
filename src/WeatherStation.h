98f1dedc7746c2db734cdc4843b705490f20c687
Jose L. Marcos <jose-louis.marcos@cnes.fr>
2024-11-22 16:26:23 +0100

    added ReST API server

--- src/WeatherStation.h
+++ src/WeatherStation.h
@@ -4,6 +4,7 @@
 
 #include <Arduino.h>
 #include <WiFiManager.h>
+#include <WebServer.h>
 #include <TFT_eSPI.h>
 #include <ArduinoJson.h>
 #include <HTTPClient.h>
@@ -11,10 +12,19 @@
 
 #define VW_Version "v. 0.01"
 
+struct sensorData {
+  float t = 0.0;                  // temp
+  float h = 0.0;                  // humidity
+  float b = 0.0;                  // battery voltage
+  bool is_update = false;         // if true, data has been updated
+};
+
 // Objects to manipulate display
 TFT_eSPI tft = TFT_eSPI();
 TFT_eSprite sprite = TFT_eSprite(&tft);
 
+// Network and Web app related
+WiFiManager wifiManager;
 String staHostname = "WeatherStation";      // Not sure it"s usefull to configure hostname.
 // [Please CHANGE THIS] - Custom Static adress for the Weather Station
 char static_ip[16] = "192.168.3.10";
@@ -22,15 +32,15 @@ char static_gw[16] = "192.168.3.1";
 char static_mask[16] = "255.255.255.0";
 char static_dns[16] = "192.168.3.1";
 
-// [Please CHANGE THIS] - API Rest server url of TempSensor (change the IP address if needed!)
-String tempSensorServer = "http://192.168.3.11/data";
+WebServer server(80);
+
+// Json var to handle JSON deserialization either of API request (POST from Temp Sensor)
+// or getting time from Internet.
 
 
-// API Web server for accurate time.
+// API Web server for accurate time getTime.
 String timeServer = "https://timeapi.io/api/time/current/zone?timeZone=Europe%2FParis";
 ESP32Time rtc(0); 
-
-unsigned long timePassed=0; // here we will store time passed
 
 struct DateTime {
   int year;
@@ -42,6 +52,9 @@ struct DateTime {
   long microsecond;
 };
 
+// forward declarations
+void setupApi();
+void handlePost();
 
 #endif 
  
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 #include "WeatherStation.h"
 #include <arduino-timer.h>
 
+//--- Sensor data var
+sensorData  fromSensor;
+
+//--- Time object
 auto timer = timer_create_default();
 
 bool getTime(void *) {
@@ -11,6 +15,13 @@ bool getTime(void *) {
 
 bool getSensor(void *) {
   Serial.println("---> In getSensor");
+  if (fromSensor.is_update) {
+    // New values have been posted, display them
+    Serial.print("Temp:"); Serial.println(fromSensor.t);
+    Serial.print("Humi:"); Serial.println(fromSensor.h);
+    Serial.print("Batt:"); Serial.println(fromSensor.b);
+    fromSensor.is_update = false;
+  }
   return true;
 } 
 
@@ -25,6 +36,27 @@ void setup() {
   Serial.print("WeatherStation");
   Serial.println(VW_Version);
 
+  //--- WiFi connection using Captive_AP.
+  wifiManager.setConfigPortalTimeout(5000);
+  IPAddress _ip,_gw,_mask,_dns;
+  _ip.fromString(static_ip);
+  _gw.fromString(static_gw);
+  _mask.fromString(static_mask);
+  _dns.fromString(static_dns);
+
+  wifiManager.setHostname(staHostname);
+  wifiManager.setSTAStaticIPConfig(_ip,_gw,_mask,_dns);
+
+  if(!wifiManager.autoConnect("AP_WifiConfig","password")) {
+    Serial.println("Failed to connect and hit timeout");
+    delay(3000);
+    ESP.restart();
+  }
+
+  Serial.println("INFO: connected to WiFi");
+
+
+  //--- Create Timers for main Weather Station functions
   timer.every(15000,getTime);              // Every 15s
   timer.every(30000,getSensor);            // Every 30s
   timer.every(60000,getForecast);          // Every 60s
@@ -32,4 +64,41 @@ void setup() {
 
 void loop() {
   timer.tick();
+}
+
+//--- setup and launch ReST API web server
+void setupApi() {
+  server.on("/post-data", HTTP_POST, handlePost);
+
+  // start Web server
+  server.begin();
+}
+
+//--- handle POST request : store new values of sensor data to fromSensor structure.
+//    Error handling is minimal : nothing is done if temp/humi/bat.level are non sense values!!
+void handlePost() {
+  JsonDocument jsonDoc;
+  char buffer[128];
+  String body;
+
+  if (server.hasArg("plain") == false) {
+    jsonDoc.clear();
+    jsonDoc["error"] = "true";
+    jsonDoc["reason"] = "no body found.";
+    serializeJson(jsonDoc,buffer);
+    
+    server.send(200,"application/json",buffer);
+  } else {
+    body = server.arg("plain");
+    deserializeJson(jsonDoc,body);
+
+    // store received data
+    fromSensor.t = jsonDoc["temperature"];
+    fromSensor.h = jsonDoc["humidity"];
+    fromSensor.b = jsonDoc["battery"];
+    fromSensor.is_update = true;
+
+    server.send(200, "application/json", "{}");
+
+  } 
 }
